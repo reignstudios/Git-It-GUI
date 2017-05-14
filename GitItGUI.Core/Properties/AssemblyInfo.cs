fcbf250451a57356d17fa77b869d7698782004bf
Andrew <andrew.witte@vrstudios.com>
2017-05-14 12:22:17 -0700

    test UI update

--- GitItGUI.Core/Properties/AssemblyInfo.cs
+++ GitItGUI.Core/Properties/AssemblyInfo.cs
@@ -37,7 +37,7 @@ using System.Runtime.InteropServices;
 
 public static class VersionInfo
 {
-	public const string version = "0.2.17";
+	public const string version = "0.2.18";
 	#if DEBUG
 	public const string versionType = version + "b";
 	#else
