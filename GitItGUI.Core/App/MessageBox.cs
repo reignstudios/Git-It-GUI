21f160b18b023e08b0a8ebbd254abebc07c5c117
Andrew <andrew.witte@vrstudios.com>
2017-06-03 14:51:47 -0700

    starting to add app core stuff on its own thread

---
+++ GitItGUI.Core/App/MessageBox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitItGUI.Core.App
+{
+	public static class MessageBox
+	{
+		public static void Show(string msg)
+		{
+			// TODO: invoke application message box
+		}
+	}
+}
---
+++ GitItGUI.Core/App/RepoPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitItGUI.Core.App
+{
+	public class RepoPage : Page
+	{
+		
+	}
+}
